b3cf16c24c77bf888cd633d88c63dbe7bfb5babc
Pattykev <trishasoftwareengineer@gmail.com>
2024-02-18 22:47:32 +0100

    initialized the spring boot project

---
+++ src/test/java/com/healthmanagerresource/HealthManager/HealthManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthmanagerresource.HealthManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
